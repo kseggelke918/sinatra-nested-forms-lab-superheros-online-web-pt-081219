f04cb19fc290f8e97420bfab48e661514fdd80aa
Katie Seggelke <katie.seggelke@gmail.com>
2019-11-15 22:30:35 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,7 +9,14 @@ class App < Sinatra::Base
     end 
     
     post '/team' do 
-      @team = Team.new 
+      @team = Team.new(params[:team])
+      
+      params[:team][:heroes].each do |details|
+        Hero.new(details)
+      end 
+      
+      @heroes = Hero.all 
+      
       erb :team 
     end 
 
